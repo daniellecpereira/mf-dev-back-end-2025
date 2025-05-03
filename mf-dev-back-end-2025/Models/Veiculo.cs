964025e1883162345979ff38455a8a559701e159
danielle.pereira <daniellecristinapereira0@gmail.com>
2025-05-02 22:59:06 -0300

    CRUD Veículo

--- mf-dev-back-end-2025/Models/Veiculo.cs
+++ mf-dev-back-end-2025/Models/Veiculo.cs
@@ -14,8 +14,10 @@ namespace mf_dev_back_end_2025.Models
         [Required(ErrorMessage = "Obrigatório informar a placa! ")]
         public string Placa { get; set; }
         [Required(ErrorMessage = "Obrigatório informar o Ano de Fabricação! ")]
+        [Display(Name ="Ano de Fabricação")]
         public int AnoFabricacao { get; set; }
         [Required(ErrorMessage = "Obrigatório informar o Ano do Modelo! ")]
+        [Display(Name = "Ano do Modelo")]
         public int AnoModelo { get; set; }
 
         
